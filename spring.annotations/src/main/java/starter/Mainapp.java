1c4dda713be16977d376e82aed1c749c259b4aa8
Doron Marcus <marcus.doron@gmail.com>
2017-01-12 12:55:36 +0200

    v2

--- spring.annotations/src/main/java/starter/Mainapp.java
+++ spring.annotations/src/main/java/starter/Mainapp.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 import beans.FirstBean;
 
@@ -11,7 +12,9 @@ import beans.FirstBean;
 public class Mainapp {
 	public static void main(String[] args) {
 		ApplicationContext context= new ClassPathXmlApplicationContext(new String[]{"Beans.xml"},false);
-		((ConfigurableEnvironment)context.getEnvironment()).setActiveProfiles("dev");
+		ConfigurableEnvironment env = (ConfigurableEnvironment)context.getEnvironment();
+		env.setActiveProfiles("dev");
+		System.out.println("my Name Property is - " + env.getProperty("myName"));
 		((ClassPathXmlApplicationContext)context).refresh();
 		
 		

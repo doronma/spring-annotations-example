c2e494b48cb4a9f9b774c1a578b6761bd24ac89c
Doron Marcus <marcus.doron@gmail.com>
2017-01-12 15:09:00 +0200

    v2

--- spring.annotations/src/main/java/starter/Mainapp.java
+++ spring.annotations/src/main/java/starter/Mainapp.java
@@ -4,26 +4,22 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
 
 import beans.FirstBean;
 
-
 public class Mainapp {
 	public static void main(String[] args) {
-		ApplicationContext context= new ClassPathXmlApplicationContext(new String[]{"Beans.xml"},false);
-		ConfigurableEnvironment env = (ConfigurableEnvironment)context.getEnvironment();
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "Beans.xml" }, false);
+		ConfigurableEnvironment env = (ConfigurableEnvironment) context.getEnvironment();
 		env.setActiveProfiles("dev");
 		System.out.println("my Name Property is - " + env.getProperty("myName"));
-		((ClassPathXmlApplicationContext)context).refresh();
-		
-		
-		FirstBean firstBean = (FirstBean)context.getBean("firstBean");
+		((ClassPathXmlApplicationContext) context).refresh();
+
+		FirstBean firstBean = (FirstBean) context.getBean("firstBean");
 		firstBean.sayHello();
-		((ConfigurableApplicationContext)context).close();
+		((ConfigurableApplicationContext) context).close();
 		System.out.println("ok");
-		
-		
+
 	}
 
 }
